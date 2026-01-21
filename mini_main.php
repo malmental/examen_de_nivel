d7fb47bb3e36394cdc2496707c967a3569ff4acd
malmental <orlandorojasoyarzun@gmail.com>
2026-01-21 09:40:53 +0100

    salto de pagina

--- mini_main.php
+++ mini_main.php
@@ -15,31 +15,3 @@ $resultado = $portal->comprendidosEntreFechas(new DateTime('2001-06-25'), new Da
 echo 'El Documento más antiguo es: ' . $documentoMasAntiguo . PHP_EOL;
 echo 'El/los documentos entre las fechas son: ' . PHP_EOL;
 var_dump($resultado);
-
-
-
-
-
-
-
-
-
-/* echo 'Portal: ' . $portal->__toString() . PHP_EOL;
-
-// ---------------------------------------
-// Documeto que lleva más tiempo archivado
-// ---------------------------------------
-$documentoMasAntiguo = $portal->documentoMasTiempoArchivado($documentos);
-echo 'El Documento que llevas mas tiempo archivado es: ' . $documentoMasAntiguo . PHP_EOL;
-
-// ----------------------------------------------------------------
-// Metodo que devuelve Documentos comprendidos entre una fecha dada
-// ----------------------------------------------------------------
-$fechaInicio = new DateTime('2001-06-24');
-$fechaFin = new DateTime('2006-05-28');
-$resultado = $portal->comparacionDeFechas($documentos, $fechaInicio, $fechaFin);
-
-echo 'Documentos encontrados entre: ' . $fechaInicio->format('Y-m-d') . ' y ' .  $fechaFin->format('Y-m-d')  . PHP_EOL;
-foreach ($resultado as $documento) {
-    echo $documento->getNombre() . PHP_EOL . $documento->getFechaRegistro()->format('Y-m-d') . PHP_EOL;
-} */
